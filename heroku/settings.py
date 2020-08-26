9570811289c4b10f4086963d24eda8ff52084480
parkaro <owaispkr@users.noreply.github.com>
2020-08-27 09:57:09 +1200

    change settings

--- heroku/settings.py
+++ heroku/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -129,4 +132,3 @@ STATIC_URL = "/static/"
 
 django_heroku.settings(locals())
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
